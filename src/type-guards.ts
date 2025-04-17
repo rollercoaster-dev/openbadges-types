f61585fad4ac70cc68ef89ff47dc4cf80ebfd71f
Joe Czarnecki <joe.czar@outlook.com>
2025-04-17 18:30:39 +0200

    feat: enhance type guards and add direct exports

--- src/type-guards.ts
+++ src/type-guards.ts
@@ -4,10 +4,18 @@
  */
 
 // Import from OB2
-import { isAssertion as isOB2Assertion, isBadgeClass as isOB2BadgeClass, isProfile as isOB2Profile } from './v2/guards';
+import {
+  isAssertion as isOB2Assertion,
+  isBadgeClass as isOB2BadgeClass,
+  isProfile as isOB2Profile,
+} from './v2/guards';
 
 // Import from OB3
-import { isVerifiableCredential as isOB3VerifiableCredential, isAchievement as isOB3Achievement, isIssuer as isOB3Issuer } from './v3/guards';
+import {
+  isVerifiableCredential as isOB3VerifiableCredential,
+  isAchievement as isOB3Achievement,
+  isIssuer as isOB3Issuer,
+} from './v3/guards';
 
 // Import from shared
 import { isJsonLdObject, hasJsonLdType, hasJsonLdContext } from './shared/jsonld';
@@ -22,19 +30,19 @@ export {
   isOB2Assertion,
   isOB2BadgeClass,
   isOB2Profile,
-  
+
   // OB3 type guards
   isOB3VerifiableCredential,
   isOB3Achievement,
   isOB3Issuer,
-  
+
   // Shared type guards
   isJsonLdObject,
   hasJsonLdType,
   hasJsonLdContext,
   isIRI,
   isDateTime,
-  
+
   // Composite type guards
   isBadge,
 };
